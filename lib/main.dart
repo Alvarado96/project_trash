b77343013029ae88a21b6f73174b9f10668ffdc9
nehemiah.alvarado_spaceforce <nehemiah.alvarad@spaceforce.mil>
2023-08-14 19:53:40 -0600

    Added a side navigation menu with a home tab and map tab using the Drawer Widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,34 @@ void main() {
         appBar: AppBar(
           backgroundColor: Color.fromARGB(255, 96, 139, 110),
         ),
+        drawer: Drawer(
+          child: ListView(
+            // Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text('Drawer Header'),
+              ),
+              ListTile(
+                title: const Text('Home'),
+                leading: const Icon(Icons.home),
+                onTap: () {
+                  // Update the state of the app
+                }
+              ),
+              ListTile(
+                title: const Text("Map"),
+                leading: const Icon(Icons.map),
+                onTap: () {
+                  // Update the state of the app
+                },
+              )
+            ],
+          ),
+        ),
       ),
     ),
   );
