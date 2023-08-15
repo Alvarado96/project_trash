11ad5bfad295ce79891fc8777c6ee52e9d0e7cce
nehemiah.alvarado_spaceforce <nehemiah.alvarad@spaceforce.mil>
2023-08-14 19:27:38 -0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 96, 139, 110),
+        ),
+      ),
+    ),
+  );
+}
